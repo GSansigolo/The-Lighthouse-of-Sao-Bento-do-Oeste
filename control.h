58fe01d5233ee67adee9020193b7d660f1c6015e
Sansigolo <gabrielsansigolo@gmail.com>
2020-03-05 22:27:42 -0300

    add neptune

---
+++ control.h
@@ -0,0 +1,78 @@
+#ifndef control
+#define control
+
+//dialog pointer
+static int start_menu = 1;
+static int choice_pause = 0;
+static int d_pointer = 1;
+
+//main controller function
+void controller(){
+    
+    //debugger
+    jo_set_printf_color_index(JO_COLOR_INDEX_White);
+    jo_printf(1, 1, "%d", d_pointer);
+    
+    //main menu
+    if (jo_is_pad1_key_down(JO_KEY_START) && start_menu == 1){
+        start_menu = 0;
+        jo_clear_screen();
+        start();
+    } 
+
+    //pointer motion trigger
+    if (jo_is_pad1_key_down(JO_KEY_A) && start_menu == 0 && choice_pause == 0){
+        jo_printf(1, 1, "%d", d_pointer);
+        d_pointer++;
+        jo_clear_screen();
+    } 
+    
+    //lips
+    if (jo_is_pad1_key_down(JO_KEY_A) && d_pointer == 6 && choice_pause == 1){
+        jo_printf(1, 1, "%d", d_pointer);
+        d_pointer = 7;
+        jo_clear_screen();
+    } 
+
+    if (jo_is_pad1_key_down(JO_KEY_B) && d_pointer == 6 && choice_pause == 1){
+        jo_printf(1, 1, "%d", d_pointer);
+        d_pointer = 8;
+        jo_clear_screen();
+    } 
+
+    if (jo_is_pad1_key_down(JO_KEY_C) && d_pointer == 6 && choice_pause == 1){
+        jo_printf(1, 1, "%d", d_pointer);
+        d_pointer = 9;
+        jo_clear_screen();
+    } 
+
+    //pointers jump
+    if (d_pointer == 2)
+        dialog_002();
+
+    if (d_pointer == 3)
+        dialog_003();
+
+    if (d_pointer == 4)
+        dialog_004();
+
+    if (d_pointer == 5)
+        dialog_005();
+
+    if (d_pointer == 6){
+        choice_pause = 1;
+        dialog_006();
+    }
+
+    if (d_pointer == 7)
+        dialog_007();
+    
+    if (d_pointer == 8)
+        dialog_008();
+    
+    if (d_pointer == 9)
+        dialog_009();
+
+}
+
+#endif
---
+++ neptune.h
@@ -0,0 +1,98 @@
+/*-------------------------------------------------------------------------------+
+|   ~+         *       +                                                         |
+|      '                  |     _   _  ___________ _____ _   _ _   _  _____      |
+|  ()    .-.,="``"=.    - o -  | \ | ||  ___| ___ \_   _| | | | \ | ||  ___|     |
+|        '=/_       \     |    |  \| || |__ | |_/ / | | | | | |  \| || |__       |
+|     *   |  '=._    |         | . ` ||  __||  __/  | | | | | | . ` ||  __|      |
+|          \     `=./`,     '  | |\  || |___| |     | | | |_| | |\  || |___      |
+|  +    .   '=.__.=' `='   *   \_| \_/\____/\_|     \_/  \___/\_| \_/\____/      |
+|                                                              BY: @G_Sansigolo  |
++-------------------------------------------------------------------------------*/
+#ifndef neptune
+#define neptune
+
+/**
+ * draw_hud()
+ * plot dialog box
+ */
+void draw_hud(){
+    add_hud("HUD.TGA", "BG");
+}
+
+/**
+ * set_background()
+ * plot background image
+ */
+void set_background(char* filename, char* folder){
+    jo_img bg;
+
+    bg.data = NULL;
+    jo_tga_loader(&bg, folder, filename, JO_COLOR_Transparent);
+    jo_set_background_sprite(&bg, 0, 0);
+    jo_free_img(&bg);
+}
+
+/**
+ * draw()
+ * draw sprite on screen
+ */
+void draw(void){
+	jo_sprite_draw3D(0, 0, 0, 500);
+}
+
+/**
+ * add_hud()
+ * add dialog box to screen
+ */
+void add_hud(char* filename, char* folder){
+    jo_sprite_add_tga(folder, filename, JO_COLOR_Green);
+	jo_core_add_callback(draw);
+}
+
+/**
+ * set_sprite()
+ * set sprite image
+ */
+void set_sprite(char* filename, char* folder){
+    jo_img sprite;
+
+    sprite.data = NULL;
+    jo_tga_loader(&sprite, folder, filename, JO_COLOR_Transparent);
+    jo_set_background_sprite(&sprite, 100, 45);
+    jo_free_img(&sprite);
+}
+
+/**
+ * d()
+ * draw text on the screen
+ */
+void d(char* text, int line){
+    jo_set_printf_color_index(JO_COLOR_INDEX_White);
+    if (line == 1)
+        jo_printf(13, 21+(2), text);
+    else
+        jo_printf(2, 21+(2*line), text);
+}
+
+/**
+ * h()
+ * draw text on the screen
+ */
+void h(char* text){
+    jo_set_printf_color_index(JO_COLOR_INDEX_Red);
+    jo_printf(2, 21+(2*1), text);
+}
+
+/**
+ * d_n()
+ * draw text on the screen
+ */
+void d_n(char* text, int line){
+    jo_set_printf_color_index(JO_COLOR_INDEX_White);
+    if (line == 1)
+        jo_printf(2, 21+(2), text);
+    else
+        jo_printf(2, 21+(2*line), text);
+}
+
+#endif
