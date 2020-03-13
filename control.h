3080ba0a452c24cf22fbbd6d26f8e5d23f56cc17
Sansigolo <gabrielsansigolo@gmail.com>
2020-03-12 23:23:41 -0300

    finish part 1

--- control.h
+++ control.h
@@ -4,7 +4,7 @@
 //dialog pointer
 static int start_menu = 1;
 static int choice_pause = 0;
-static int d_pointer = 1;
+static int d_pointer = 999;
 
 //main controller function
 void controller(){
@@ -17,24 +17,44 @@ void controller(){
     if (jo_is_pad1_key_down(JO_KEY_START) && start_menu == 1){
         start_menu = 0;
         jo_clear_screen();
+        d_pointer = 998;
         start();
     } 
 
     //pointer motion trigger
     if (jo_is_pad1_key_down(JO_KEY_A) && start_menu == 0 && choice_pause == 0){
         jo_printf(1, 1, "%d", d_pointer);
-        d_pointer++;
+        if (d_pointer == 998)
+            d_pointer = 0;
+        else
+            d_pointer++;
         jo_clear_screen();
     } 
     
     //pointer motion with background
-    if (jo_is_pad1_key_down(JO_KEY_A) && start_menu == 0 && choice_pause == 0 && d_pointer == 8){
+    if (jo_is_pad1_key_down(JO_KEY_A) && start_menu == 0 && choice_pause == 0 && d_pointer == 7){
         jo_clear_screen();
-        dialog_009();
-        d_pointer = 10;
+        dialog_008();
+        d_pointer = 9;
     } 
-    
+
+    if (jo_is_pad1_key_down(JO_KEY_A) && start_menu == 0 && choice_pause == 0 && d_pointer == 13){
+        jo_clear_screen();
+        dialog_014();
+        d_pointer = 15;
+    } 
+
     //pointers jump
+    if (d_pointer == 998){
+        dialog_0000();
+    }
+
+    if (d_pointer == 0)
+        dialog_000();
+
+    if (d_pointer == 1)
+        dialog_001();
+
     if (d_pointer == 2)
         dialog_002();
 
@@ -56,6 +76,9 @@ void controller(){
     if (d_pointer == 8)
         dialog_008();
 
+    if (d_pointer == 10)
+        dialog_010();
+
     if (d_pointer == 11)
         dialog_011();
 
@@ -67,7 +90,124 @@ void controller(){
     
     if (d_pointer == 14)
         dialog_014();
-              
+
+    if (d_pointer == 15)
+        dialog_015();
+
+    if (d_pointer == 16)
+        dialog_016();
+
+    if (d_pointer == 17)
+        dialog_017();
+
+    if (d_pointer == 18)
+        dialog_018();
+
+    if (d_pointer == 19)
+        dialog_019();
+
+    if (d_pointer == 20)
+        dialog_020();
+
+    if (d_pointer == 21)
+        dialog_021();
+
+    if (d_pointer == 22)
+        dialog_022();
+
+    if (d_pointer == 23)
+        dialog_023();
+    
+    if (d_pointer == 24)
+        dialog_024();
+
+    if (d_pointer == 25)
+        dialog_025();
+
+    if (d_pointer == 26)
+        dialog_026();
+
+    if (d_pointer == 27)
+        dialog_027();
+
+    if (d_pointer == 28)
+        dialog_028();
+
+    if (d_pointer == 29)
+        dialog_029();
+
+    if (d_pointer == 30)
+        dialog_030();
+
+    if (d_pointer == 31)
+        dialog_031();
+
+    if (d_pointer == 32)
+        dialog_032();
+
+    if (d_pointer == 33)
+        dialog_033();
+    
+    if (d_pointer == 34)
+        dialog_034();
+
+    if (d_pointer == 35)
+        dialog_035();
+
+    if (d_pointer == 36)
+        dialog_036();
+
+    if (d_pointer == 37)
+        dialog_037();
+
+    if (d_pointer == 38)
+        dialog_038();
+
+    if (d_pointer == 39)
+        dialog_039();  
+
+    if (d_pointer == 40)
+        dialog_040();  
+
+    if (d_pointer == 41)
+        dialog_041();
+
+    if (d_pointer == 42)
+        dialog_042();
+
+    if (d_pointer == 43)
+        dialog_043();
+    
+    if (d_pointer == 44)
+        dialog_044();
+
+    if (d_pointer == 45)
+        dialog_045();
+
+    if (d_pointer == 46)
+        dialog_046();
+
+    if (d_pointer == 47)
+        dialog_047();
+
+    if (d_pointer == 48)
+        dialog_048();
+
+    if (d_pointer == 49)
+        dialog_049(); 
+
+    if (d_pointer == 50)
+        dialog_050();
+
+    if (d_pointer == 51)
+        dialog_051(); 
+
+    if (d_pointer == 52)
+        dialog_052();  
+
+    if (d_pointer == 53)
+        dialog_053();  
+                      
 }
 
 #endif
--- model.h
+++ model.h
@@ -11,98 +11,383 @@ void draw_menu(void){
 
 void start(void){
     jo_clear_screen();
-    set_background("BLACK.TGA", "BG");
-    dialog_001();
+    set_background("BLACK.TGA", "BG"); //CARTA
+    dialog_0000();
 }
 
-void dialog_001(void){
+void dialog_0000(void){
   d_n("You are Renne Bras, an ordinary",1); 
     d("detective. You entered the business",2); 
     d("seven years ago, just after the",3); 
 }
 
-
-void dialog_002(void){
+void dialog_000(void){
   d_n("end of your longest relationship.",1); 
     d("You became famous for solving the",2); 
     d("case of kidnappings in the capital.",3); 
 }
 
-void dialog_003(void){
+void dialog_001(void){
+  d_n("Ever since you never picked up",1); 
+    d("jobs that involved disappearance ",2); 
+    d("cases.",3); 
+}
+
+void dialog_002(void){
   d_n("Everything changed when days ago",1); 
     d("you received a message from an old",2); 
     d("friend.",3); 
 }
 
-void dialog_004(void){
+void dialog_003(void){
   d_n("Renne long time no see, I am sending",1); 
     d("this message because I am running ",2); 
     d("out of hope. A couple of my friends",3); 
 }
 
-void dialog_005(void){
+void dialog_004(void){
   d_n("went missing a week ago. I called",1); 
     d("the police but they were unable to",2); 
     d("find evidence to proceed.",3); 
 }
 
-void dialog_006(void){
+void dialog_005(void){
   d_n("I would like to hire you to investi-",1); 
     d("gate this case. I live in Sao Bento",2); 
     d("do Oeste, Minas Gerais, near the",3); 
 }
 
-void dialog_007(void){
+void dialog_006(void){
   d_n("border with Sao Paulo. Please help",1); 
     d("me Renne, you are my only hope.",2); 
     d("With love, - Julia.",3); 
 }
 
-void dialog_008(void){
+void dialog_007(void){
   d_n("Normally, you never accepted jobs",1); 
     d("outside the state of Sao Paulo, but",2); 
     d("this time you made an exception.",3); 
 }
 
-void dialog_009(void){
+void dialog_008(void){
     jo_clear_screen();
-    set_background("SAIDA.TGA", "BG");    
-    dialog_010();
+    set_background("SAIDA.TGA", "BG"); //SAIDA COM RENNE
+    dialog_009();
 }
 
-void dialog_010(void){
+void dialog_009(void){
 
   d_n("Sao Bento do Oeste",1); 
     d("Minas Gerais - Brazil",2); 
-    d("Morning",3); 
+    d("City entrance",3); 
 }
 
-void dialog_011(void){  
+void dialog_010(void){  
     h("[ Renne ]");
                 d("Hello Sao Bento do Oeste.",1);
     d("I hope you have a good case for me.",2); 
     d("",3);
 }
 
-void dialog_012(void){  
+void dialog_011(void){  
     h("[ Renne ]");
                 d("...",1);
     d("",2); 
     d("",3);
 }
 
-void dialog_013(void){  
+void dialog_012(void){  
     h("[ Renne ]");
-                d("Okay, now where's Julia.",1);
+                d("Ok, now where is Julia.",1);
     d("",2); 
     d("",3);
 }
 
-void dialog_014(void){  
+void dialog_013(void){  
     h("[ Julia ]");
-                d("Renneeeee!",1);
+                d("Rennee!",1);
     d("",2); 
     d("",3);
+}
+
+void dialog_014(void){
+    jo_clear_screen();
+    set_background("SAIDA.TGA", "BG"); //SAIDA COM RENNE E JULIA
+    dialog_015();
+}
+
+void dialog_015(void){  
+    h("[ Julia ]");
+                d("You came!",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_016(void){  
+    h("[ Renne ]");
+                d("I couldn't ignore an old",1);
+    d("friend.",2); 
+    d("",3);
+}
+
+void dialog_017(void){  
+    h("[ Julia ]");
+                d("Long time no see.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_018(void){  
+    h("[ Renne ]");
+                d("Yes, it does.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_019(void){  
+    h("[ Renne ]");
+                d("I would love to spend an",1);
+    d("afternoon talking with you...",2); 
+    d("",3);
+}
+
+void dialog_020(void){  
+    h("[ Julia ]");
+                d("Right, the case.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_021(void){  
+    h("[ Renne ]");
+                d("I will ask you a few ",1);
+    d("questions, feel free to ask back",2); 
+    d("anytime.",3);
+}
+
+void dialog_022(void){  
+    h("[ Julia ]");
+                d("Okay.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_023(void){  
+    h("[ Renne ]");
+                d("Who are the couple of",1);
+    d("friends you mentioned?",2); 
+    d("",3);
+}
+
+void dialog_024(void){  
+    h("[ Julia ]");
+                d("Their names are Cristiano",1);
+    d("Machado and Alice Zena. I have known ",2); 
+    d("them for three years.",3);
+}
+
+void dialog_025(void){  
+    h("[ Julia ]");
+                d("Are they married, friends ",1);
+    d("or something else?",2); 
+    d("",3);
+}
+
+void dialog_026(void){  
+    h("[ Julia ]");
+                d("They are dating. I",1);
+    d("remember Alice telling me that she",2); 
+    d("was waiting for Cris to ask her..",3);
+}
+
+void dialog_027(void){  
+    h("[ Renne ]");
+                d("I see. What was their jobs?",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_028(void){  
+    h("[ Julia ]");
+                d("Cristiano was a lawyer,",1);
+    d("he had an office on 15th March Street,",2); 
+    d("the main street of the city.",3);
+}
+
+void dialog_029(void){  
+    h("[ Julia ]");
+                d("Alice was a Portuguese ",1);
+    d("teacher at the school I work at.",2); 
+    d("",3);
+}
+
+void dialog_030(void){  
+    h("[ Renne ]");
+                d("How would you describe",1);
+    d("their relationship?",2); 
+    d("",3);
+}
+
+void dialog_031(void){  
+    h("[ Julia ]");
+                d("They seemed very close, ",1);
+    d("a perfect couple, I never saw them ",2); 
+    d("fight...",3);
+}
+
+void dialog_032(void){  
+    h("[ Renne ]");
+                d("Hum.. ",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_033(void){  
+    h("[ Julia ]");
+                d("I'm sorry did I say",1);
+    d("something?",2); 
+    d("",3);
+}
+
+void dialog_034(void){  
+    h("[ Renne ]");
+                d("No, I just remembered one",1);
+    d("thing... Please continue.",2); 
+    d("",3);
+}
+
+void dialog_035(void){  
+    h("[ Julia ]");
+                d("They were... I mean they",1);
+    d("are a beautiful couple.",2); 
+    d("",3);
+}
+
+void dialog_036(void){  
+    h("[ Renne ]");
+                d("As I understand it, you",1);
+    d("were much closer to Alice than to",2); 
+    d("Cristiano right?",3);
+}
+
+void dialog_037(void){  
+    h("[ Julia ]");
+                d("Yes.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_038(void){  
+    h("[ Renne ]");
+                d("About Cristiano, you knew ",1);
+    d("something about his life beyond what ",2); 
+    d("Alice told you...",3);
+}
+
+void dialog_039(void){  
+    h("[ Julia ]");
+                d("As far as I remember he",1);
+    d("used to go to drink in the bar in ",2); 
+    d("front of his office.",3);
+}
+
+void dialog_040(void){  
+    h("[ Renne ]");
+                d("Did you knew Cristiano",1);
+    d("before his relationship with Alice?",2); 
+    d("",3);
+}
+
+void dialog_041(void){  
+    h("[ Julia ]");
+                d("Just by sight.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_042(void){  
+    h("[ Renne ]");
+                d("Right. About the",1);
+    d("disappearance, how you noticed that",2); 
+    d("both were gone.",3);
+}
+
+void dialog_043(void){  
+    h("[ Julia ]");
+                d("They don't have a car, so",1);
+    d("I saw them every day walking to work.",2); 
+    d("",3);
+}
+
+void dialog_044(void){  
+    h("[ Julia ]");
+                d("A week ago Alice said that ",1);
+    d("she wanted to go to the Lighthouse.",2); 
+    d("",3);
+}
+
+void dialog_045(void){  
+    h("[ Renne ]");
+                d("Lighthouse?",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_046(void){  
+    h("[ Julia ]");
+                d("Yes, the City Lighthouse.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_047(void){  
+    h("[ Renne ]");
+                d("Why does this city have",1);
+    d("a lighthouse? We are in Minas-Gerais,",2); 
+    d("there no sea.",3);
+}
+
+void dialog_048(void){  
+    h("[ Julia ]");
+                d("It is the city monument,",1);
+    d("they built it to be a tourist spot for",2); 
+    d("the city.",3);
+}
+
+
+void dialog_049(void){  
+    h("[ Renne ]");
+                d("Anyway, let's continue.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_050(void){  
+    h("[ Julia ]");
+                d("Alice said she would go",1);
+    d("to the Lighthouse. Once I heard it, I",2); 
+    d("thought that Cristiano would ask her.",3);
+}
+
+void dialog_051(void){  
+    h("[ Renne ]");
+                d("Oh.",1);
+    d("",2); 
+    d("",3);
+}
+
+void dialog_052(void){  
+    h("[ Julia ]");
+                d("Since then I have not ",1);
+    d("heard any more news from the two, ",2); 
+    d("both have stopped showing up at their",3);
+}
+
+void dialog_053(void){  
+    h("[ Julia ]");
+                d("work, the house has been ",1);
+    d("abandoned, and from what I talked,",2); 
+    d("nobody has seen them since ever since.",3);
 }
 
 #endif
